079a4a3909b625c293ace4b2cd8b6fb296c53acd
Sarah Schaller <sarcsch@aol.com>
2017-11-06 18:06:34 +0000

    automatically backed up by learn

---
+++ app/models/click_bait_validator.rb
@@ -0,0 +1,4 @@
+# class ClickBaitValidator < ActiveModel::EachValidator
+#   def validate_each(post, title, value)
+#     if !value.nil? 
+#       unless
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maxiumum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-fiction"]}
 end
