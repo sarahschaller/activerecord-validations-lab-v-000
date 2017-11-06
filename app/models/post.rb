6f592a57c992c9c2ce55fea2c0e0be08e34a6b59
Sarah Schaller <sarcsch@aol.com>
2017-11-06 18:15:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maxiumum: 250 }
   validates :category, inclusion: { in: ["Fiction", "Non-fiction"]}
+  validates :title, click_bait: true
 end
